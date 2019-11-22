6fb40a180ee6546e6b990abd834b1f35dcec366d
Jasim Atiyeh <jasimatiyeh@gmail.com>
2019-11-22 15:24:29 -0600

    add images

--- dist/bundle.js
+++ dist/bundle.js
@@ -182,6 +182,19 @@ const imageSelection = (titleBanner, imageSelector, puzzle, difficulty) => {
   });
 };
 
+// export const Images = {
+//   image1: 'https://puzzleology.s3.us-east-2.amazonaws.com/A2_SigmaPhiEpsilon-900x600.jpg',
+//   image2: 'https://puzzleology.s3.us-east-2.amazonaws.com/5718269042_8b2181bed8_o-900x600.png',
+//   image3: 'https://puzzleology.s3.us-east-2.amazonaws.com/A2_SigmaPhiEpsilon-900x600.jpg',
+//   image4: 'https://puzzleology.s3.us-east-2.amazonaws.com/KIlworth-House-Hotel_0003-3-900x600.jpeg',
+//   image5: 'https://puzzleology.s3.us-east-2.amazonaws.com/KIlworth-House-Hotel_0003-3-900x600.jpg',
+//   image6: 'https://puzzleology.s3.us-east-2.amazonaws.com/Wayne_campus_scienglib_900x600.jpg',
+//   image7: 'https://puzzleology.s3.us-east-2.amazonaws.com/bahia-de-las-aguilas-pedernales-900x600.jpg',
+//   image8: 'https://puzzleology.s3.us-east-2.amazonaws.com/heidi-kaden-n7NwHLauoJ0-unsplash-900x600.jpg',
+//   image9: 'https://puzzleology.s3.us-east-2.amazonaws.com/march_moon.900x600.jpg',
+//   image10: 'https://puzzleology.s3.us-east-2.amazonaws.com/spice-of-life-900x600.jpg'
+// };
+
 const Images = {
   image1: '../../assets/images/111919_Events-900x600.jpg',
   image2: '../../assets/images/5718269042_8b2181bed8_o-900x600.png',
